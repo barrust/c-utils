9225499598a8ef9d32ee02669f35618a8144f56b
Tyler Barrus <barrust@gmail.com>
2019-12-09 17:42:06 -0500

    update tests to not pass if a test failed

--- tests/timing_test.c
+++ tests/timing_test.c
@@ -94,5 +94,6 @@ MU_TEST_SUITE(test_suite) {
 int main(int argc, char *argv[]) {
     MU_RUN_SUITE(test_suite);
     MU_REPORT();
-    return 0;
+    printf("Number failed tests: %d\n", minunit_fail);
+    return minunit_fail;
 }
