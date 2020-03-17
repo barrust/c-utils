8e1f9d487909cc2ab245557272e9f6131d6447e8
Tyler Barrus <barrust@gmail.com>
2020-03-16 23:47:02 -0400

    additional test coverage

--- tests/queue_test.c
+++ tests/queue_test.c
@@ -16,6 +16,12 @@ void test_teardown(void) {
     q = NULL;
 }
 
+int* make_into_pointer(int i) {
+    int* t = malloc(sizeof(int));
+    *t = i;
+    return t;
+}
+
 /*******************************************************************************
 *   Setup
 *******************************************************************************/
@@ -30,13 +36,13 @@ MU_TEST(test_default_setup) {
 *   Push
 *******************************************************************************/
 MU_TEST(test_simple_pushes) {
-    q_push(q, NULL);
+    q_push(q, make_into_pointer(0));
     mu_assert_int_eq(1, q_num_elements(q));
     mu_assert_not_null(q_first_node(q));
     mu_assert_not_null(q_last_node(q));
     mu_assert_pointers_eq(q_first_node(q), q_last_node(q));
 
-    q_push(q, NULL);
+    q_push(q, make_into_pointer(1));
     mu_assert_int_eq(2, q_num_elements(q));
     mu_assert_not_null(q_first_node(q));
     mu_assert_not_null(q_last_node(q));
@@ -47,19 +53,21 @@ MU_TEST(test_simple_pushes) {
 *   Pop
 *******************************************************************************/
 MU_TEST(test_simple_pops) {
-    q_push(q, NULL);
-    q_push(q, NULL);
-    q_push(q, NULL);
+    q_push(q, make_into_pointer(0));
+    q_push(q, make_into_pointer(1));
+    q_push(q, make_into_pointer(2));
     mu_assert_int_eq(3, q_num_elements(q));
 
     void* tail = q_last_node(q);
-    q_pop(q);
+    int* i = (int*)q_pop(q);
     mu_assert_int_eq(2, q_num_elements(q));
+    mu_assert_int_eq(*i, 0);
+    free(i);
     /* make sure we haven't moved the tail... */
     mu_assert_pointers_eq(tail, q_last_node(q));
     mu_assert_pointers_not_eq(tail, q_first_node(q));
 
-    q_pop(q);
+    q_pop_alt(q, true);
     mu_assert_int_eq(1, q_num_elements(q));
     /*  make sure we haven't moved the tail and that now, with only one node
         that the head and tail are the same */
@@ -68,10 +76,27 @@ MU_TEST(test_simple_pops) {
 
     tail = NULL;
 
-    q_pop(q);
+    q_pop_alt(q, true);
     mu_assert_int_eq(0, q_num_elements(q));
     mu_assert_null(q_first_node(q));
     mu_assert_null(q_last_node(q));
+
+    /* we should be empty queue, try popping again! */
+    mu_assert_null(q_pop(q));
+}
+
+/*******************************************************************************
+*   Free
+*******************************************************************************/
+MU_TEST(test_queue_free) {
+    queue_list_t tmp = q_init();
+    q_push(tmp, NULL);
+    q_push(tmp, NULL);
+    q_push(tmp, NULL);
+
+    mu_assert_int_eq(3, q_num_elements(tmp));
+    /* since there is nothing allocated outside we can call the standard free */
+    q_free(tmp);
 }
 
 /*******************************************************************************
@@ -83,6 +108,7 @@ MU_TEST_SUITE(test_suite) {
     MU_RUN_TEST(test_default_setup);
     MU_RUN_TEST(test_simple_pushes);
     MU_RUN_TEST(test_simple_pops);
+    MU_RUN_TEST(test_queue_free);
 }
 
 
--- tests/stack_test.c
+++ tests/stack_test.c
@@ -15,6 +15,12 @@ void test_teardown(void) {
     stk = NULL;
 }
 
+int* make_into_pointer(int i) {
+    int* t = malloc(sizeof(int));
+    *t = i;
+    return t;
+}
+
 /*******************************************************************************
 *   Setup
 *******************************************************************************/
@@ -28,7 +34,7 @@ MU_TEST(test_default_setup) {
 *   Push
 *******************************************************************************/
 MU_TEST(test_simple_pushes) {
-    stk_push(stk, NULL);
+    stk_push(stk, make_into_pointer(0));
     mu_assert_int_eq(1, stk_num_elements(stk));
     mu_assert_not_null(stk_first_node(stk));
 
@@ -36,7 +42,7 @@ MU_TEST(test_simple_pushes) {
 
     mu_assert_pointers_eq(stk_first_node(stk), cur);
 
-    stk_push(stk, NULL);
+    stk_push(stk, make_into_pointer(1));
     mu_assert_int_eq(2, stk_num_elements(stk));
     mu_assert_not_null(stk_first_node(stk));
     mu_assert_pointers_not_eq(stk_first_node(stk), cur);
@@ -48,19 +54,35 @@ MU_TEST(test_simple_pushes) {
 *   Pop
 *******************************************************************************/
 MU_TEST(test_simple_pops) {
-    stk_push(stk, NULL);
-    stk_push(stk, NULL);
-    stk_push(stk, NULL);
+    stk_push(stk, make_into_pointer(0));
+    stk_push(stk, make_into_pointer(1));
+    stk_push(stk, make_into_pointer(2));
     mu_assert_int_eq(3, stk_num_elements(stk));
 
-    stk_pop(stk);
+    int* i = stk_pop(stk);
     mu_assert_int_eq(2, stk_num_elements(stk));
+    mu_assert_int_eq(2, *i);
+    free(i);
 
-    stk_pop(stk);
+    stk_pop_alt(stk, true);
     mu_assert_int_eq(1, stk_num_elements(stk));
-    stk_pop(stk);
+    stk_pop_alt(stk, true);
     mu_assert_int_eq(0, stk_num_elements(stk));
     mu_assert_null(stk_first_node(stk));
+}
+
+/*******************************************************************************
+*   Free
+*******************************************************************************/
+MU_TEST(test_stack_free) {
+    stack_list_t tmp = stk_init();
+    stk_push(tmp, NULL);
+    stk_push(tmp, NULL);
+    stk_push(tmp, NULL);
+
+    mu_assert_int_eq(3, stk_num_elements(tmp));
+    /* since there is nothing allocated outside we can call the standard free */
+    stk_free(tmp);
 }
 
 /*******************************************************************************
@@ -72,6 +94,7 @@ MU_TEST_SUITE(test_suite) {
     MU_RUN_TEST(test_default_setup);
     MU_RUN_TEST(test_simple_pushes);
     MU_RUN_TEST(test_simple_pops);
+    MU_RUN_TEST(test_stack_free);
 }
 
 
